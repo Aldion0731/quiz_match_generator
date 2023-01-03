041fbb5454ab905a88fd272b6880817191b01d19
Aldion Lee <lxdavid.lee@gmail.com>
2023-01-03 22:30:47 +0900

    Add code for creating quiz seasons from individual excel files
    Add code for cleaning season data

---
+++ src/utilities/cleaner.py
@@ -0,0 +1,53 @@
+import re
+from typing import List
+
+import pandas as pd
+
+
+class Cleaner:
+    def clean(self, text: str, remove_chars: List[str]) -> str:
+        text = self.remove_unwanted_terminal_chars(text, remove_chars)
+        return self.rename_areas(text)
+
+    def remove_unwanted_terminal_chars(self, text: str, chars: List[str] = [""]) -> str:
+        text = text.rstrip(" ")
+        for char in chars:
+            text = text.rstrip(char)
+        return text
+
+    def rename_areas(self, text: str):
+        text = self.rename_bio(text)
+        text = self.rename_mus(text)
+        text = self.rename_phy(text)
+        return self.rename_spo(text)
+
+    def rename_bio(self, text: str) -> str:
+        pattern = re.compile(r"bio\b")
+        return pattern.sub("Bio", text)
+
+    def rename_phy(self, text: str) -> str:
+        pattern = re.compile(r"Phy\b")
+        return pattern.sub("Phys", text)
+
+    def rename_spo(self, text: str) -> str:
+        pattern = re.compile(r"(Spo\b)|(Sport\b)")
+        return pattern.sub("Sports", text)
+
+    def rename_mus(self, text: str) -> str:
+        pattern = re.compile(r"Mus\b")
+        return pattern.sub("Music", text)
+
+
+class DfCleaner(Cleaner):
+    def get_clean_df(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.copy()
+        df = df.dropna()
+        df.columns = self.get_clean_col_names(df)
+        df["Area"] = self.get_clean_area_col(df)
+        return df
+
+    def get_clean_col_names(self, df: pd.DataFrame) -> List[str]:
+        return [self.remove_unwanted_terminal_chars(col) for col in df.columns]
+
+    def get_clean_area_col(self, df: pd.DataFrame) -> pd.Series:
+        return df["Area"].apply(lambda x: self.clean(x, remove_chars=["."]))
---
+++ src/utilities/quiz_seasons.py
@@ -0,0 +1,52 @@
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, List
+
+import pandas as pd
+
+from .cleaner import DfCleaner
+
+
+@dataclass
+class QuizSeason:
+    alternate: pd.DataFrame = pd.DataFrame()
+    minutes: pd.DataFrame = pd.DataFrame()
+    buzzer: pd.DataFrame = pd.DataFrame()
+
+    def from_read_excel(self, read_excel: Dict) -> QuizSeason:
+        section_keys: List[str] = list(read_excel.keys())
+        return QuizSeason(
+            alternate=read_excel[section_keys[0]],
+            minutes=read_excel[section_keys[1]],
+            buzzer=read_excel[section_keys[2]],
+        )
+
+    def with_clean_data(self, df_cleaner: DfCleaner) -> QuizSeason:
+        df_cleaner = DfCleaner()
+        return QuizSeason(
+            alternate=df_cleaner.get_clean_df(self.alternate),
+            minutes=df_cleaner.get_clean_df(self.minutes),
+            buzzer=df_cleaner.get_clean_df(self.buzzer),
+        )
+
+
+class ExcelSeasons:
+    def load_from_files(self, data_dir: Path) -> List[Dict]:
+        season_files = self.__get_season_files(data_dir)
+        return self.__load_excel_seasons(season_files)
+
+    def __load_excel_seasons(self, season_files: List[Path]) -> List[Dict]:
+        excel_seasons: List[Dict] = []
+        for file in season_files:
+            try:
+                excel_season = pd.read_excel(file, sheet_name=None, header=0)
+                excel_seasons.append(excel_season)
+            except Exception:
+                continue
+        return excel_seasons
+
+    def __get_season_files(self, data_dir: Path) -> List[Path]:
+        return [data_dir / file for file in os.listdir(data_dir)]
