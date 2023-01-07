75ee44c94c41a11d9b7c5524f763e82f88689ab1
Aldion Lee <lxdavid.lee@gmail.com>
2023-01-07 21:59:56 +0900

    Add Enum for list of subjects

---
+++ src/utilities/subjects.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+
+class Subjects(Enum):
+    BK = "BK"
+    BIO = "Bio"
+    ENG = "Eng"
+    CA = "CA"
+    CHEM = "Chem"
+    MUS = "Music"
+    GK = "GK"
+    SPO = "Sports"
+    SPA = "Span"
+    GEO = "Geo"
+    MAT = "Math"
+    JH = "JH"
+    IT = "IT"
+    HIS = "His"
+    THE = "The"
+    PHY = "Phys"
+    FRE = "French"
+    LIT = "Lit"
+    ART = "Art"
