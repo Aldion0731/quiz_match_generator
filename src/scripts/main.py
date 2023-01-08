bc785594a6e08706c85987cc1b8f189936082d8b
Aldion Lee <lxdavid.lee@gmail.com>
2023-01-08 16:03:48 +0900

    Allow custom section lengths to be set in the config

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -1,5 +1,6 @@
 from dataclasses import asdict
 from datetime import datetime
+from pathlib import Path
 
 import pandas as pd
 
@@ -10,7 +11,10 @@ from ..utilities.section_banks import SectionBanks, SectionFilter
 from ..utilities.sections_builder import SectionBuilder
 
 
-def run(config: Config, match_round: int) -> None:
+def run(config: Config, match_round: int | None = None) -> None:
+    if match_round is None:
+        match_round = proj_config.settings.default_match_round
+
     excel_seasons = ExcelSeasons().load_from_files(config.filepaths.data_dir)
     seasons = [QuizSeason().from_read_excel(season) for season in excel_seasons]
 
@@ -22,15 +26,23 @@ def run(config: Config, match_round: int) -> None:
     round_bank = section_banks.from_round(match_round)
 
     alternate_builder = get_section_builder(round_bank.alternate, 4, 2)
-    alternate = pd.DataFrame(asdict(alternate_builder.get_paired_questions()))
+    alternate = pd.DataFrame(asdict(alternate_builder.get_paired_questions())).iloc[
+        : config.num_round_questions.first
+    ]
 
     minutes_builder = get_section_builder(round_bank.minutes, 6, 2)
-    minutes = pd.DataFrame(asdict(minutes_builder.get_paired_questions()))
+    minutes = pd.DataFrame(asdict(minutes_builder.get_paired_questions())).iloc[
+        : config.num_round_questions.second
+    ]
 
     buzzer_builder = get_section_builder(round_bank.buzzer, 2, 1)
-    buzzer = pd.DataFrame(asdict(buzzer_builder.get_unpaired_questions()))
+    buzzer = pd.DataFrame(asdict(buzzer_builder.get_unpaired_questions())).iloc[
+        : config.num_round_questions.third
+    ]
 
-    generate_match(alternate, minutes, buzzer, match_round)
+    match_path = get_match_path(config.filepaths.generated_matches, match_round)
+
+    generate_match(alternate, minutes, buzzer, match_path)
 
 
 def get_section_builder(
@@ -43,15 +55,24 @@ def get_section_builder(
 
 
 def generate_match(
-    alternate: pd.DataFrame, minutes: pd.DataFrame, buzzer: pd.DataFrame, round: int
+    alternate: pd.DataFrame,
+    minutes: pd.DataFrame,
+    buzzer: pd.DataFrame,
+    match_path: Path,
 ) -> None:
-    date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    with pd.ExcelWriter(f"round-{round}_{date}.xlsx", engine="xlsxwriter") as writer:
+    with pd.ExcelWriter(  # pylint: disable=abstract-class-instantiated
+        match_path, engine="xlsxwriter"
+    ) as writer:
         alternate.to_excel(writer, sheet_name="Alternate")
         minutes.to_excel(writer, sheet_name="Minutes")
         buzzer.to_excel(writer, sheet_name="Buzzer")
 
 
+def get_match_path(dest_dir: Path, match_round) -> Path:
+    date = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    return dest_dir / f"round-{match_round}_{date}.xlsx"
+
+
 if __name__ == "__main__":
-    config = load_config()
-    run(config, 5)
+    proj_config = load_config()
+    run(proj_config)
